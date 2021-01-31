d222824c56bbf19dd61cb263cc98e09b24d01965
sukanta7660 <spurkayastha83@gmail.com>
2021-01-31 13:27:40 +0600

    Appointment page designed

--- resources/views/user/appointment.blade.php
+++ resources/views/user/appointment.blade.php
@@ -7,8 +7,8 @@
         <div class="container">
             <div class="row no-gutters slider-text align-items-center justify-content-center">
                 <div class="col-md-9 ftco-animate text-center">
-                    <p class="breadcrumbs"><span class="mr-2"><a href="#">Home</a></span> <span>Contact</span></p>
-                    <h1 class="mb-0 bread">Contact Us</h1>
+                    <p class="breadcrumbs"><span class="mr-2"><a href="#">Home</a></span> <span>Doctors</span></p>
+                    <h1 class="mb-0 bread">Appointment</h1>
                 </div>
             </div>
         </div>
@@ -19,28 +19,17 @@
                 <div class="w-100"></div>
                 <div class="col-md-3 d-flex">
                     <div class="info bg-white p-4">
-                        <p><span>Address:</span>1st floor,example,Sylhet</p>
-                    </div>
+                        <h2>Doctor Info</h2>
+                        <img src="{{asset('public/uploads/doctor/'.$table->imageName)}}" alt="doctor image" style="max-width: 100%;border: 2px solid #ccc;padding: 6px;">
+                        <h5><span>Name: </span> {{$table->name}}</h5>
+                        <p><span>Chamber: </span> {{$table->chamberDetails}}</p>
+                        <p><span>Check Fee: </span> {{money($table->checkFee)}}</p>
+                        <p><span>Time: </span> {{$table->time}}</p>
                 </div>
-                <div class="col-md-3 d-flex">
-                    <div class="info bg-white p-4">
-                        <p><span>Phone:</span> <a href="tel://1234567920">+01616161551</a></p>
-                    </div>
                 </div>
-                <div class="col-md-3 d-flex">
-                    <div class="info bg-white p-4">
-                        <p><span>Email:</span> <a href="mailto:info@yoursite.com">info@oursite.com</a></p>
-                    </div>
-                </div>
-                <div class="col-md-3 d-flex">
-                    <div class="info bg-white p-4">
-                        <p><span>Website</span> <a href="#">oursite.com</a></p>
-                    </div>
-                </div>
-            </div>
-            <div class="row block-9">
-                <div class="col-md-6 order-md-last d-flex">
+                <div class="col-md-9 order-md-last d-flex">
                     <form action="#" class="bg-white p-5 contact-form">
+                        <h2>Get Your Appointment</h2>
                         <div class="form-group">
                             <input type="text" class="form-control" placeholder="Your Name">
                         </div>
@@ -54,14 +43,10 @@
                             <textarea name="" id="" cols="30" rows="7" class="form-control" placeholder="Message"></textarea>
                         </div>
                         <div class="form-group">
-                            <input type="submit" value="Send Message" class="btn btn-primary py-3 px-5">
+                            <button type="submit" class="btn btn-primary">Submit</button>
                         </div>
                     </form>
 
-                </div>
-
-                <div class="col-md-6 d-flex">
-                    <div id="map" class="bg-white"></div>
                 </div>
             </div>
         </div>
